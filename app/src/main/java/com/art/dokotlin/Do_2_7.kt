6c04681e651c7c44a695e79aaecc2761a500f6a9
QiuCheng <cheng.qiu@gamesvessel.com>
2022-06-22 14:44:36 +0800

    Learn Kotlin Do Demo

---
+++ app/src/main/java/com/art/dokotlin/Do_2_7.kt
@@ -0,0 +1,16 @@
+package com.art.dokotlin
+
+/**
+ * Created by cheng.qiu on 2022/05/07
+ * Description:
+ */
+class Do_2_7 {
+    // Kotlin
+    fun add(a: Int, b: Int): Int {
+        return a + b
+    }
+
+    fun add01(a: Int, b: Int): Int = a + b
+
+
+}
