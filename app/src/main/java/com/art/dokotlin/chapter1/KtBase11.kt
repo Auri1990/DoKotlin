1a84b2b58cea59dc1f751573e4a4564d25cccaa4
QiuCheng <cheng.qiu@gamesvessel.com>
2022-07-06 15:39:29 +0800

    Kotlin 反编译为 字节码 Java代码

---
+++ app/src/main/java/com/art/dokotlin/chapter1/KtBase11.kt
@@ -0,0 +1,16 @@
+package com.art.dokotlin.chapter1
+
+/**
+ * Created by cheng.qiu on 2022/07/06
+ * Description: TODO 11.查看 Kotlin 反编译后的字节码
+ * Tools -> Kotlin -> Show Kotlin Bytecode = 查看提供给 JVM 的字节码 -> Decompile = 反编译成 Java
+ */
+
+const val PI1 = 3.1415
+const val PI2 = 3.1415
+const val age = 988
+
+fun main() {
+    val name = "Auri"
+    val ID = 1024
+}
