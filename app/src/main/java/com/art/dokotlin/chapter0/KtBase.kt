6cc3986917494c9f827b9c54f8f76a0fb9ab889f
QiuCheng <cheng.qiu@gamesvessel.com>
2022-10-12 20:05:22 +0800

    ddd

--- app/src/main/java/com/art/dokotlin/chapter0/KtBase.kt
+++ app/src/main/java/com/art/dokotlin/chapter0/KtBase.kt
@@ -1,16 +1,10 @@
 package com.art.dokotlin.chapter0
 
 /**
- * Created by cheng.qiu on 2022/07/06
- * Description: TODO 08.Kotlin 语言的只读变量
+ * Description: TODO .Kotlin
+ * Conclusion:
  */
 
 fun main(){
-    // var 可读可改； val 不可改只可读
-    val info : String = "Auri is coding"
-    println(info)
 
-    var age : Int = 34
-    age = 35
-    println(age)
 }
---
+++ app/src/main/java/com/art/dokotlin/chapter3/KtBase45.kt
@@ -0,0 +1,34 @@
+package com.art.dokotlin.chapter3
+
+/**
+ * Description: TODO 45.Kotlin replace 实现简易字符串加解密
+ * Conclusion:
+ */
+
+fun main() {
+    val srcPwd = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    println("sourcePassword: $srcPwd")
+
+    val newPwd = srcPwd.replace(Regex("[ACIS]")) {
+        it.value // 这行代码没有做任何事
+        when (it.value) { // 这里相当于遍历每一个字母
+            "A" -> "1"
+            "C" -> "2"
+            "I" -> "3"
+            "S" -> "4"
+            else -> it.value // 不做变化
+        }
+    }
+    println("newPassword: $newPwd")
+
+    val recoverPwd = newPwd.replace(Regex("[1234]")) {
+        when (it.value) {
+            "1" -> "A"
+            "2" -> "C"
+            "3" -> "I"
+            "4" -> "S"
+            else -> it.value
+        }
+    }
+    println("recoverPassword: $recoverPwd")
+}
